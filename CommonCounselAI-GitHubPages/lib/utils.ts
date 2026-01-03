65e8113fcd3a1b71ad5716f1e65b29d07f4bd414
lawbuddyai-creator <lawbuddyai@gmail.com>
2026-01-03 12:21:42 -0500

    save

--- CommonCounselAI-GitHubPages/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function cx(...classes: Array<string | false | null | undefined>) {
-  return classes.filter(Boolean).join(" ");
-}
-
-export function formatUSD(n: number) {
-  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(n);
-}
